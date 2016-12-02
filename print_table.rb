22687ffdebb1267df9fe7a8e97bae0afbe45d267
Mike Barile <mikebarile13@gmail.com>
2016-12-02 14:13:49 -0800

    Succesfully formatted table

--- print_table.rb
+++ print_table.rb
@@ -2,23 +2,36 @@
 def print_table(table)
 
   #Determine formatting constants
-  table_width = table[-1].map{ |el| el.to_s }.join(" ").length
-  max_prime_width = table[0][-1].to_s.length + 1
+  #Used to format first column
+  max_prime_width = table[0][-1].to_s.length + 2
+  #Used to format entire table width
+  table_width = table[-1].map{ |el| el.to_s }.join("  ").length - max_prime_width + 1
+  #Used to determine spacing of elements inside of table
+  idx_widths = []
+  table[-1].each{ |el| idx_widths << el.to_s.length + 1}
 
   #Print header
   puts ''
-  max_prime_width.times{print " "}
-  table[0].each {|el| print "#{el} "}
+  (max_prime_width + 1).times {print " "}
+  table[0].each_with_index do |el, idx|
+    print "#{el}"
+    num_spaces = idx_widths[idx + 1] - el.to_s.length + 1
+    num_spaces.times {print " "}
+  end
   puts ''
-  max_prime_width.times{print " "}
+  max_prime_width.times {print " "}
   table_width.times {print "-"}
   puts ''
 
   #Print remaining rows
   table[1..-1].each do |row|
-    print "#{row[0]}| "
-    row[1..-1].each do |el|
-      print "#{el} "
+    print "#{row[0]}"
+    (max_prime_width - row[0].to_s.length - 1).times {print " "}
+    print "| "
+    row[1..-1].each_with_index do |el, idx|
+      print "#{el}"
+      num_spaces = idx_widths[idx + 1] - el.to_s.length + 1
+      num_spaces.times {print " "}
     end
     puts ''
   end
